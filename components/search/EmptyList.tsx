25cf9f6faea69d9511b38d46961aa5984aa3230b
Hwang Sujung <crystalhwang0416@gmail.com>
2024-01-21 01:29:58 +0900

    feat: 빈 페이지일 경우 핸들링 컴포넌트 추가

--- components/search/EmptyList.tsx
+++ components/search/EmptyList.tsx
@@ -1,7 +1,7 @@
 const EmptyList = () => (
   <div className="">
-    <h2 className="">빈 페이지입니다</h2>
-    <span className="">키워드를 입력해주세요</span>
+    <h2 className="">검색 결과가 없습니다</h2>
+    <span className="">다른 키워드를 입력해주세요</span>
   </div>
 );
 export default EmptyList;
